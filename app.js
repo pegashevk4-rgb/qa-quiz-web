9e65cff18a1b9d8045e18dfc34ccbafcb884eb63
«Кирилл <pegashev.k@yandex.ru>
2026-05-31 13:27:54 +0300

    Fixed

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 // =========================
 
 // Базовый URL бэкенда
-const API_BASE_URL = "http://127.0.0.1:8000";
+const API_BASE_URL = "http://localhost:8000";
 
 // =========================
 // URL PARAMS
